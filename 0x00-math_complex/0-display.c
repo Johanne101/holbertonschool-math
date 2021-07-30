95af224d5d0151bfae8e04334d9bd92a98a7fc5b
johanne101 <jlopez0493@gmail.com>
2021-07-30 22:00:08 +0000

    [modified] 0 task file

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,15 +6,20 @@
  */
 void display_complex_number(complex c)
 {
+	double abs = c.im;
+
 	printf("%.f", c.re);
 	if (c.im != 0)
 	{
 		if (c.im > 0)
 			printf(" + ");
 		else
+		{
 			printf(" - ");
-		if (c.im != -1 && c.im != 1)
-			printf("%.f", c.im);
+			abs = c.im * -1;
+		}
+		if (abs != 1)
+			printf("%.f", abs);
 		printf("i");
 	}
 	printf("\n");
