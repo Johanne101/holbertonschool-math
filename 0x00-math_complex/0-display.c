f8d9737940091e1e46dacf8bde5b8d2da6659cd2
johanne101 <jlopez0493@gmail.com>
2021-07-30 21:40:51 +0000

    [updt] output format of im section

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,8 +6,14 @@
  */
 void display_complex_number(complex c)
 {
-	if (c.im == 0)
-		printf("%.f\n", c.re);
-	else
-	printf("%.f + %.fi\n", c.re, c.im);
+	printf("%.f", c.re);
+	if (c.im != 0)
+	{
+		if (c.im > 0)
+			printf(" + ");
+		else
+			printf(" - ");
+		printf("%.fi", c.im);
+	}
+	printf("\n");
 }
